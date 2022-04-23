817f3a960ee68f89a08fa35e7cfaa410feead796
Aman Verma <amaverma3@publicisgroupe.net>
2022-04-23 17:49:13 +0530

    header changes

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,7 +1,21 @@
 function Category(props) {
   const { name, description, imageUrl, key, order } = props.category;
   const isOddOrder = order % 2 == 0 ? false : true;
-  return <div></div>;
+  return (
+    <div className="container">
+      <div
+        className="category row"
+        style={{ flexDirection: `${isOddOrder ? "row" : "row-reverse"}` }}
+      >
+        <img src={imageUrl} alt={`image of ${name}`} />
+        <div className="col category-details">
+          <h4>{name}</h4>
+          <p>{description}</p>
+          <button className="explore-btn">Explore {key}</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Category;
