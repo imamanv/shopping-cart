4a3475d3fa2039ffb2ec2fd3d5825af93a2294f1
Aman Verma <amaverma3@publicisgroupe.net>
2022-04-23 15:39:32 +0530

    fix layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
-    <footer className="footer">
-      <p>Copyright &#169; 2011-2018 Sabka Bazaar Grocery Supplies Pvt Ltd </p>
+    <footer>
+      <div className="container">
+        <p>Copyright &#169; 2011-2018 Sabka Bazaar Grocery Supplies Pvt Ltd </p>
+      </div>
     </footer>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,30 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <>
-      <header className="header">
-        <nav className="navbar">
-          <Link to="/">
-            <img className="nav-brand-img" src="static/images/logo.png" />
-          </Link>
-          <div className="nav-links">
-            <div className="prod-links">
-              <Link to="/">Home</Link>
-              <Link to="/products">Products</Link>
-            </div>
-            <div className="login-links">
-              <Link to="/signin">SignIn</Link>
-              <Link to="/register">Register</Link>
+    <header>
+      <div className="container">
+        <nav className="row">
+          <div className="col col-1">
+            <img
+              src="static\images\logo.png"
+              alt="Brand Logo of Sabka Bazaar"
+            />
+          </div>
+          <div className="col col-2">
+            <Link to="/">Home</Link>
+            <Link to="/products">Products</Link>
+          </div>
+          <div className="col col-3">
+            <Link to="/">SingIn</Link>
+            <Link to="/products">Register</Link>
+            <div className="cart-info">
+              <img src="static\images\cart.svg" alt="cart icon" />
+              <p>0 items</p>
             </div>
           </div>
         </nav>
-      </header>
-    </>
+      </div>
+    </header>
   );
 }
 
