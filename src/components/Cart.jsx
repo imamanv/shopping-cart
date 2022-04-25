a6625848894ec5a43f4ebe0f5cf0b29b4840cdbd
Aman Verma <amaverma3@publicisgroupe.net>
2022-04-26 00:56:46 +0530

    few more changes

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,56 +1,54 @@
 function Cart() {
   return (
-    <div className="container">
-      <div className="cart">
-        <div className="cart-title">
-          <p>My Cart (0 item)</p>
-          <p className="cart-close-icon">&times;</p>
-        </div>
-        <div className="cart-items">
-          <div className="cart-item">
-            <img
-              src="static\images\products\fruit-n-veg\apple.jpg"
-              alt=""
-              height="120px"
-            />
-            <div className="cart-item-info">
-              <h4>Apple - Washington, Regular, 4 pcs</h4>
-              <p>Price</p>
-            </div>
-          </div>
-          <div className="cart-item">
-            <img
-              src="static\images\products\fruit-n-veg\apple.jpg"
-              alt=""
-              height="120px"
-            />
-            <div className="cart-item-info">
-              <h4>Apple - Washington, Regular, 4 pcs</h4>
-              <p>Price</p>
-            </div>
-          </div>
-          <div className="cart-item">
-            <img
-              src="static\images\products\fruit-n-veg\apple.jpg"
-              alt=""
-              height="120px"
-            />
-            <div className="cart-item-info">
-              <h4>Apple - Washington, Regular, 4 pcs</h4>
-              <p>Price</p>
-            </div>
-          </div>
-          <div className="cart-banner">
-            <img src="static\images\lowest-price.png" alt="" />
-            <p>You won't find it cheaper anywhere</p>
+    <div className="cart">
+      <div className="cart-title">
+        <p>My Cart (0 item)</p>
+        <p className="cart-close-icon">&times;</p>
+      </div>
+      <div className="cart-items">
+        <div className="cart-item">
+          <img
+            src="static\images\products\fruit-n-veg\apple.jpg"
+            alt=""
+            height="120px"
+          />
+          <div className="cart-item-info">
+            <h4>Apple - Washington, Regular, 4 pcs</h4>
+            <p>Price</p>
           </div>
         </div>
-        <div className="cart-footer">
-          <p>Promo can be applied on payment page</p>
-          <div className="proceed-section">
-            <p>Proceed to Checkout</p>
-            <p>Rs.187 &gt;</p>
+        <div className="cart-item">
+          <img
+            src="static\images\products\fruit-n-veg\apple.jpg"
+            alt=""
+            height="120px"
+          />
+          <div className="cart-item-info">
+            <h4>Apple - Washington, Regular, 4 pcs</h4>
+            <p>Price</p>
           </div>
+        </div>
+        <div className="cart-item">
+          <img
+            src="static\images\products\fruit-n-veg\apple.jpg"
+            alt=""
+            height="120px"
+          />
+          <div className="cart-item-info">
+            <h4>Apple - Washington, Regular, 4 pcs</h4>
+            <p>Price</p>
+          </div>
+        </div>
+        <div className="cart-banner">
+          <img src="static\images\lowest-price.png" alt="" />
+          <p>You won't find it cheaper anywhere</p>
+        </div>
+      </div>
+      <div className="cart-footer">
+        <p>Promo can be applied on payment page</p>
+        <div className="proceed-section">
+          <p>Proceed to Checkout</p>
+          <p>Rs.187 &gt;</p>
         </div>
       </div>
     </div>
